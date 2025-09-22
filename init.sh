9363496d50917e2093b7eb1a02082487e16f8b8a
Fabian Weichselbaum <fweichselbaum@barracuda.com>
2025-09-22 15:51:30 +0200

    Add Zsh

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+ln -s $PWD/zsh/zshrc ~/.zshrc
+ln -s $PWD/tmux/tmux.conf ~/.tmux.conf
+
 ln -s $PWD/foot ~/.config/foot
 ln -s $PWD/nvim ~/.config/nvim
-
-ln -s $PWD/tmux/tmux.conf ~/.tmux.conf
