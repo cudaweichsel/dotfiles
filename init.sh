a5eac7769b8491c26e26c04fd2fd75ef8a734110
Fabian Weichselbaum <fweichselbaum@barracuda.com>
2025-09-24 14:10:56 +0200

    Add git config

--- init.sh
+++ init.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 ln -s $PWD/zsh/zshrc ~/.zshrc
+ln -s $PWD/git/gitconfig ~/.gitconfig
 ln -s $PWD/tmux/tmux.conf ~/.tmux.conf
 
-ln -s $PWD/foot ~/.config/foot
+ln -s $PWD/foot/foot.ini ~/.config/foot/foot.ini
 ln -s $PWD/nvim ~/.config/nvim
