9efbd772fd2dce1c7e0dd0d9977d609bb53ee879
Fabian Weichselbaum <fweichselbaum@barracuda.com>
2025-09-29 13:25:05 +0200

    XDG_config_home for tmux

--- init.sh
+++ init.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-ln -s $PWD/zsh/zshrc ~/.zshrc
-ln -s $PWD/git/gitconfig ~/.gitconfig
-ln -s $PWD/tmux/tmux.conf ~/.tmux.conf
+ln -s $PWD/zsh/zshrc      ~/.zshrc
 
-ln -s $PWD/foot/foot.ini ~/.config/foot/foot.ini
-ln -s $PWD/nvim ~/.config/nvim
+ln -s $PWD/git/config     ~/.config/git/config
+ln -s $PWD/tmux/tmux.conf ~/.config/tmux/tmux.conf
+ln -s $PWD/foot/foot.ini  ~/.config/foot/foot.ini
+
+ln -s $PWD/nvim           ~/.config/nvim
